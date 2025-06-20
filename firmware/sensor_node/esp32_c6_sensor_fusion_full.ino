eb0bf333d191fe4ea4c6f4a8ec40e6adbd318acb
AJ-Shipp <anthonyjamesshipp@gmail.com>
2025-06-20 12:53:50 -0500

    Added comments to esp32_c6_sensor_fusion_full.ino file

--- firmware/sensor_node/esp32_c6_sensor_fusion_full.ino
+++ firmware/sensor_node/esp32_c6_sensor_fusion_full.ino
@@ -10,33 +10,58 @@
 #include <driver/usb_serial_jtag.h>
 #include <esp_sleep.h>
 
-#define BATTERY_PIN       0
-#define DIAG_PIN          2
-#define WIND_PIN          3
-#define SCK               4
-#define MOSI              5
-#define REGULATOR_EN_PIN  7
-#define RGB_BUILTIN       8
-#define MISO              15
-#define BME_CS            23
-#define RFID_CS           22
-#define LORA_CS           21
-#define LORA_RST          20
-#define LORA_IRQ          18
+// ---------- Pin Definitions ----------
+#define BATTERY_PIN       0     // Battery Level Pin (ADC-in)
+#define DIAG_PIN          2     // Diagnostic Pin 
+#define WIND_PIN          3     // Wind Speed Pin (ADC-in)
+#define SCK               4     // SPI Serial Clock Pin
+#define MOSI              5     // SPI Master-Out-Slave-In Pin
+#define REGULATOR_EN_PIN  7     // 5V Regulator Pin
+#define RGB_BUILTIN       8     // Built-in RGB LED Pin
+#define MISO              15    // SPI Master-In-Slave-Out Pin
+#define BME_CS            23    // BME280 Chip Select Pin for SPI 
+#define RFID_CS           22    // RFID (MRFC522) Chip Select Pin for SPI 
+#define LORA_CS           21    // LoRa Chip Select Pin for SPI 
+#define LORA_RST          20    // LoRa Chip Reset Pin
+#define LORA_IRQ          18    // LoRa Chip Interrupt Request Pin
 
-#define RF95_FREQ 915.0
-#define us_S 1000000ULL
-#define sleep_Time 600
+#define RF95_FREQ 915.0         // RFID Frequency
+#define us_S 1000000ULL         // 
+#define sleep_Time 600          // Define sleep time
 #define LORA_NODE_ID "NODE_01"  // Manually assigned LoRa ID
 
+/*
+Initiation of functions/hardware
+
+bme() = Sets up I2C interface for BME280
+rfidPin() = 
+rfidDriver() = 
+mfrc522() = 
+radio() = 
+*/
 Adafruit_BME280 bme(BME_CS);
 MFRC522DriverPinSimple rfidPin(RFID_CS);
 MFRC522DriverSPI rfidDriver(rfidPin);
 MFRC522 mfrc522(rfidDriver);
 RH_RF95 radio(LORA_CS, LORA_IRQ);
 
-RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR int bootCount = 0;  // Counts the number of times the node starts up
 
+/*
+Node info & Data variable initiation
+
+ID = Device ID
+Web_App_Path = Specific spreadsheet ID for autoprocessing and upload to Google Sheets
+DataURL = Same as Web_App_Path
+Status_Read_Sensor = Boolean status for if the sensors were readable  
+rfid = Optional RFID tag UID
+Temp = Temperature in C
+Humd = Relative humidity as a percentage
+Prs = Pressure in hPa
+batteryVoltage 
+batteryPercent
+windSpeed = Speed of wind in m/s
+*/
 String ID = "01.19";
 String Web_App_Path = "/macros/s/AKfycbzyinhl792ozO-Szi3PwUx6tGTd164YckAYTUUvRkZu2g-h25iGzGqcjx3fGKGF6NEs/exec";
 String DataURL = "", Status_Read_Sensor = "", rfid = "";
@@ -45,11 +70,39 @@ float batteryVoltage = 0.0, batteryPercent = 0.0, windSpeed = 0.0;
 
 volatile bool diagRequested = false;
 
+/*
+Function that grabs the battery percentage
+
+Parameters:
+voltage = Float value
+
+Work:
+voltage = Input value constrained to between 2.0 and 3.0 (=2.0 if < 2.0, =3.0 if > 3.0)
+
+Output: 
+Returns a percentage of the constrained input voltage's magnitude difference from the lower bound
+*/
 float getBatteryPercentage(float voltage) {
   voltage = constrain(voltage, 2.0, 3.0);
   return (voltage - 2.0) * 100.0;
 }
 
+/*
+Function that flashes an RGB LED
+
+Parameters: 
+r = Red value between 0 and 255
+g = Green value between 0 and 255
+b = Blue value between 0 and 255
+count = Max value for iterator (how many times it repeats) 
+delayMs = Amount of time between the LED flashes
+
+Work: 
+- Flashes the LED with the input RGB value
+- Delay
+- Flashes the LED white
+- Delay
+*/
 void flashRGB(uint8_t r, uint8_t g, uint8_t b, int count = 1, int delayMs = 300) {
   for (int i = 0; i < count; i++) {
     rgbLedWrite(RGB_BUILTIN, r, g, b);
@@ -59,6 +112,17 @@ void flashRGB(uint8_t r, uint8_t g, uint8_t b, int count = 1, int delayMs = 300)
   }
 }
 
+/*
+Function that attempts to connect to the wifi hotspot
+
+Work: 
+WiFi.mode(WIFI_STA) = Sets the WiFi to station mode (the ESP32 connects to an access point)
+WiFi.setSleep(false) = Takes the ESP32 out of the WiFi power save sleep mode
+WiFi.begin(arg1,arg2) = Starts attempting to connect to a network
+- arg1 = network SSID
+- arg2 = network password
+while() = While the WiFi is not connected and the millis passed subtracted by the start time is less than 800, delay by 0.5 seconds
+*/
 void connectToHotspot() {
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false);
@@ -67,10 +131,44 @@ void connectToHotspot() {
   while (WiFi.status() != WL_CONNECTED && millis() - start < 8000) delay(500);
 }
 
+/*
+Function in the ESP32's RAM that turns the diagnostic button on
+*/
 void IRAM_ATTR onDiagButton() {
   diagRequested = true;
 }
 
+/*
+Function that runs diagnostic checks of various systems and flashes LED error codes
+
+Parameters: 
+bmeOK = Boolean for if the BME is functional
+rfidOK = Boolean for if the RFID is functional
+battPct = Float value of the battery percentage
+wifiOK = Boolean for if the WiFi is functional
+loraOK = Boolean for if the LoRaWAN is functional
+
+Work: 
+- Flashes the LED white twice 150 milliseconds apart
+- BME-RFID Test:
+(Is BME Functional?)|(Is RFID Functional?)|(Output LED Color)
+False               | False               | Red
+True                | False               | Yellow
+False               | True                | Blue
+True                | True                | Green
+- Battery Percentage Test:
+(Is there a Battery Percentage?)|(What is larger than_)|(Output LED Color)
+False                           | N/a                  | Red
+True                            | 40%                  | Yellow
+True                            | 75%                  | Green
+- WiFi-LoRaWAN Test: 
+(Is WiFi Functional?)|(Is LoRaWAN Functional?)|(Output LED Color)
+False                | False                  | Red
+True                 | False                  | Yellow
+False                | True                   | Blue
+True                 | True                   | Green
+- Set diagRequested to false
+*/
 void runDiagnostics(bool bmeOK, bool rfidOK, float battPct, bool wifiOK, bool loraOK) {
   flashRGB(255, 255, 255, 2, 150);
   if (bmeOK && rfidOK) flashRGB(0, 255, 0);
@@ -87,9 +185,23 @@ void runDiagnostics(bool bmeOK, bool rfidOK, float battPct, bool wifiOK, bool lo
   else if (loraOK) flashRGB(0, 0, 255);
   else flashRGB(255, 0, 0);
   diagRequested = false;
-
 }
 
+/*
+Function called to enter/check the diagnostics mode
+
+Parameters: 
+bmeOK = Boolean for if the BME is functional
+rfidOK = Boolean for if the RFID is functional
+battPct = Float value of the battery percentage
+wifiOK = Boolean for if the WiFi is functional
+loraOK = Boolean for if the LoRaWAN is functional
+
+Work: 
+- Initializes the diagnostic pin
+- If the USB is inserted the diagnostics are ran, and a 2 second delay occurs
+- If the diagnostic pin is pulled low, a 2 second delay occurs, then if the pin is still low it runs the diagnostics
+*/
 void checkDiagnosticsMode(bool bmeOK, bool rfidOK, float battPct, bool wifiOK, bool loraOK) {
   pinMode(DIAG_PIN, INPUT_PULLUP);
   if (usb_serial_jtag_is_connected()) {
@@ -103,6 +215,15 @@ void checkDiagnosticsMode(bool bmeOK, bool rfidOK, float battPct, bool wifiOK, b
   }
 }
 
+/*
+Function to setsup 
+
+Work: 
+- Initializes the regulator enable pin as an output GPIO pin
+- Writes the regulatory enable pin high 
+- Initializes the GPIO hold disable pin
+- Initializes the GPIO hold enable pin
+*/
 void prepareRegulatorPin() {
   pinMode(REGULATOR_EN_PIN, OUTPUT);
   digitalWrite(REGULATOR_EN_PIN, HIGH);
@@ -110,6 +231,21 @@ void prepareRegulatorPin() {
   gpio_hold_en((gpio_num_t)REGULATOR_EN_PIN);
 }
 
+/*
+Function to encode a string as a url
+
+Parameters: 
+s = Input string to be encoded
+
+Work: 
+- Initializes the 'encoded' string variable
+- Initializes the constant 'hex' character variable
+- For each character 'c' in 's', 
+  - If 'c' is alphanumeric, add 'c' to 'encoded'
+  - Else, add '%' to 'encoded', add hex[(c >> 4) & 0xF] to 'encoded', add hex[c & 0xF] to 'encoded'
+Output: 
+Returns 'encoded'
+*/
 String urlEncode(const String& s) {
   String encoded = "";
   const char* hex = "0123456789ABCDEF";
@@ -124,6 +260,15 @@ String urlEncode(const String& s) {
   return encoded;
 }
 
+/*
+Function to build the website URL
+
+Parameters: 
+diagnosticMode = Boolean that tells if diagnostic mode is enabled (defaults to false)
+
+Work: 
+
+*/
 void buildURL(bool diagnosticMode = false) {
   DataURL = Web_App_Path;
   DataURL += diagRequested ? "?sts=diag" : "?sts=write";
@@ -139,6 +284,28 @@ void buildURL(bool diagnosticMode = false) {
   DataURL += "&lnid=" + String(LORA_NODE_ID);  // LoRa Node ID
 }
 
+/*
+Function to read the sensor data
+
+Work: 
+  digitalWrite(BME_CS, LOW);              #ESP32 initiates SPI communication with BME 
+  Temp = bme.readTemperature();           #^Polls the temperature sensor
+  Humd = bme.readHumidity();              #^Polls the humidity sensor
+  Prs  = bme.readPressure() / 100.0F;     #^Polls the pressure sensor, and converts the reading to hectoPascals
+  digitalWrite(BME_CS, HIGH);             #^ESP32 ends SPI communication with BME
+  Status_Read_Sensor = (isnan(Temp) || isnan(Humd) || isnan(Prs)) ? "Failed" : "Success"; #Verifies that each variable holds a number
+
+  digitalWrite(RFID_CS, LOW);             #ESP32 initiates SPI communication with RFID (MFRC522 reader/writer)  
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {   
+    rfid = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+      rfid += String(mfrc522.uid.uidByte[i], HEX);
+  }
+  ^^ Verifies the Proximity Integrated Circuit Card (PICC) exists and that it can be read
+  ^^ Initiates 'rfid' tag variable
+  ^^ Converts the HEX rfid tag and updates the 'rfid' variable 
+  digitalWrite(RFID_CS, HIGH);            #^ESP32 ends SPI communication with RFID (MFRC522 reader/writer) 
+*/
 void readSensors() {
   digitalWrite(BME_CS, LOW);
   Temp = bme.readTemperature();
@@ -156,6 +323,16 @@ void readSensors() {
   digitalWrite(RFID_CS, HIGH);
 }
 
+/*
+Function to send 
+
+Work: 
+  HTTPClient http;                                                  #Initializes the 'http' object as an instance of the 'HTTPClient' class
+  String url = "http://192.168.4.1/data?url=" + urlEncode(DataURL); #Sets 'url' to the IP address for the host
+  http.begin(url);                                                  #Establishes connection
+  http.GET();                                                       #Pulls the Server Name, Server Port, URLPath, HTTP Method, and User Agent
+  http.end();                                                       #Ends connection
+*/
 void HTTP_send() {
   HTTPClient http;
   String url = "http://192.168.4.1/data?url=" + urlEncode(DataURL);
@@ -164,6 +341,45 @@ void HTTP_send() {
   http.end();
 }
 
+/*
+LoRa_sendAndVerify() function that sends a message over LoRa, then verifies that the message was received
+
+Work: 
+  digitalWrite(LORA_RST, LOW);                #Initiates reset of LoRa device
+  delay(10); 
+  digitalWrite(LORA_RST, HIGH);               #Ends reset of LoRa device
+  delay(10);
+  if (!radio.init()) {
+    Serial.println("❌ LoRa init failed");
+    return false;                             #If the LoRa device failts to initiate, the function returns false
+  }
+
+  radio.setFrequency(RF95_FREQ);                              #Sets 'radio' frequency to previously specified value
+  radio.setTxPower(20, false);                                #Sets transmission power to 20 (false means we have a boost)
+  radio.send((uint8_t *)DataURL.c_str(), DataURL.length());   #Sends 'DataURL' as a string
+  radio.waitPacketSent();                                     #^Blocks until the transmitter is no longer transmitting.
+
+  // Wait for ACK
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);                #Defines 'len' as max buffer size for RH_RF95
+
+  if (radio.waitAvailableTimeout(1000)) {         #Starts the receiver and blocks for a 1s
+    if (radio.recv(buf, &len)) {                  #Turns the receiver on if it not already 
+      Serial.print("✅ ACK received: ");           on. If there is a valid message available, 
+      Serial.println((char*)buf);                   copy it to buf and return true else return false.
+      return true;
+    } else {
+      Serial.println("⚠️ ACK receive failed");
+      return false;
+    }
+  } else {
+    Serial.println("⚠️ No ACK received");
+    return false;
+  }
+
+Output:
+- Returns either true or false
+*/
 bool LoRa_sendAndVerify() {
   digitalWrite(LORA_RST, LOW);
   delay(10);
@@ -176,7 +392,7 @@ bool LoRa_sendAndVerify() {
 
   radio.setFrequency(RF95_FREQ);
   radio.setTxPower(20, false);
-  radio.send((uint8_t *)DataURL.c_str(), DataURL.length());
+  radio.send((uirnt8_t *)DataURL.c_st(), DataURL.length());
   radio.waitPacketSent();
 
   // Wait for ACK
@@ -198,6 +414,40 @@ bool LoRa_sendAndVerify() {
   }
 }
 
+/*
+LoRa_pingAndWaitForPong() function
+
+Work: 
+  if (!radio.init()) {
+    Serial.println("❌ LoRa init failed");
+    return false;                                             #If the LoRa device failts to initiate, the function returns false
+  }
+
+  radio.setFrequency(RF95_FREQ);                              #Sets 'radio' frequency to previously specified value
+  radio.setTxPower(20, false);                                #Sets transmission power to 20 (false means we have a boost)
+  radio.send((uint8_t *)"ping", 4);                           #Sends 'DataURL' as a string
+  radio.waitPacketSent();                                     #^Blocks until the transmitter is no longer transmitting.
+
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);                                  #Defines 'len' as max buffer size for RH_RF95
+
+  if (radio.waitAvailableTimeout(1000)) {                     #Starts the receiver and blocks for a 1s
+    if (radio.recv(buf, &len)) {                              #Turns the receiver on if it not already on. If there is a valid 
+  //                                                           message available, copy it to buf and return true else return false.
+      String reply = String((char*)buf).substring(0, len);    #Intakes the bufferred message, setting it to the 'reply' variable
+      if (reply == "pong") {                                  #^Checks if this string is "pong"
+        Serial.println("✅ Pong received from LoRa");
+        return true;
+      }
+    }
+  }
+
+  Serial.println("⚠️ Pong not received");
+  return false;                                               #^If the string is not "pong", it returns false
+
+Output: 
+- Returns either true or false
+*/
 bool LoRa_pingAndWaitForPong() {
   if (!radio.init()) {
     Serial.println("❌ LoRa init failed");
